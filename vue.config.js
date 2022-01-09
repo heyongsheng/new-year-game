34018661c0bad8dc78089626ff7167113aff6a93
贺永胜 <1378431028@qq.com>
2022-01-10 00:53:11 +0800

    添加打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 贺永胜
+ * @Date: 2022-01-09 22:38:36
+ * @email: 1378431028@qq.com
+ * @LastEditors: 贺永胜
+ * @LastEditTime: 2022-01-10 00:46:35
+ * @Descripttion: 
+ */
 module.exports = {
   publicPath: "/game/", //打包后部署在一个子路径上http:xxx/m/
   productionSourceMap: false
