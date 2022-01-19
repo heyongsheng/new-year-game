889b250fb8fb46d400408d0bafe9b9c27a0f349b
贺永胜 <1378431028@qq.com>
2022-01-19 08:59:02 +0800

    使用本地背景音乐

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@
  * @Date: 2022-01-07 22:37:16
  * @email: 1378431028@qq.com
  * @LastEditors: 贺永胜
- * @LastEditTime: 2022-01-19 01:31:04
+ * @LastEditTime: 2022-01-19 08:57:36
  * @Descripttion: 
  */
 import Vuex from 'vuex'
@@ -11,7 +11,8 @@ import Vue from 'vue'
 
 Vue.use(Vuex)
 window.backMusic = new Audio()
-window.backMusic.src = 'https://m10.music.126.net/20220119015517/48d52d8000d38590024e7538d641c3f0/yyaac/obj/wonDkMOGw6XDiTHCmMOi/3064195211/8e9b/95ca/28ac/8d329b9e8180f0ba6234bf1e784995ed.m4a'
+// window.backMusic.src = 'https://m10.music.126.net/20220119015517/48d52d8000d38590024e7538d641c3f0/yyaac/obj/wonDkMOGw6XDiTHCmMOi/3064195211/8e9b/95ca/28ac/8d329b9e8180f0ba6234bf1e784995ed.m4a'
+window.backMusic.src = require('@/assets/mp3/back.mp3')
 window.backMusic.loop = true
 window.backMusic.load()
 window.backMusic.currentTime = 127.2 // 背景音乐默认定位到舒缓片段
